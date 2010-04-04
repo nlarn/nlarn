4f1a783cd999c5b655719000aa74727dce677997
Johanna Ploog <johanna.ploog@googlemail.com>
2010-04-04 00:36:46 +0000

    Overhaul item prices to make price id much less of a no-brainer. Numbers may need tweaking.
    git-svn-id: https://nlarn.svn.sourceforge.net/svnroot/nlarn/trunk@550 561b6253-61b6-49b5-99ee-40376865e0bd

--- src/rings.c
+++ src/rings.c
@@ -25,13 +25,13 @@ const ring_data rings[RT_MAX] =
 {
     /* type            name                  effect           price bo */
     { RT_NONE,         "",                   ET_NONE,            0, 0, },
-    { RT_REGENERATION, "regeneration",       ET_INC_HP_REGEN,  220, 0, },
+    { RT_REGENERATION, "regeneration",       ET_INC_HP_REGEN,  250, 0, },
     { RT_PROTECTION,   "protection",         ET_PROTECTION,    150, 1, },
-    { RT_ENERGY,       "energy",             ET_INC_MP_REGEN,  180, 0, },
-    { RT_DEXTERITY,    "dexterity",          ET_INC_DEX,       120, 1, },
-    { RT_STRENGTH,     "strength",           ET_INC_STR,        85, 1, },
-    { RT_CLEVERNESS,   "cleverness",         ET_INC_INT,       120, 1, },
-    { RT_INC_DAMAGE,   "increase damage",    ET_INC_DAMAGE,    125, 0, },
+    { RT_ENERGY,       "energy",             ET_INC_MP_REGEN,  250, 0, },
+    { RT_DEXTERITY,    "dexterity",          ET_INC_DEX,       110, 1, },
+    { RT_STRENGTH,     "strength",           ET_INC_STR,       110, 1, },
+    { RT_CLEVERNESS,   "cleverness",         ET_INC_INT,       110, 1, },
+    { RT_INC_DAMAGE,   "increase damage",    ET_INC_DAMAGE,    150, 0, },
     { RT_EXTRA_REGEN,  "extra regeneration", ET_INC_HP_REGEN, 1000, 0, },
 };
 
