1f0d65a50feb11403d5ae2b66a79c7f12608dcc1
Joachim de Groot <jdegroot@web.de>
2011-01-11 17:30:06 +0000

    Added missing references to assert.h.
    git-svn-id: https://nlarn.svn.sourceforge.net/svnroot/nlarn/trunk@903 561b6253-61b6-49b5-99ee-40376865e0bd

--- src/fov.c
+++ src/fov.c
@@ -16,6 +16,7 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <string.h>
 #include "fov.h"
 #include "map.h"
--- src/wrap_monsters.c
+++ src/wrap_monsters.c
@@ -16,6 +16,7 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <stdlib.h>
