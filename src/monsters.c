nlarn/nlarn

commit 9801467c1ea2a6087576d37f1756d5260834190b

Fire resistance dependent on monster level

src/monsters.c
@@ -1601,14 +1601,13 @@ monster *monster_damage_take(monster *m, damage *dam)
 
     int hp_orig = m->hp;
 
-    /* FIXME: implement resistances */
     switch (dam->type)
     {
     case DAM_PHYSICAL:
         dam->amount -= monster_ac(m);
         break;
 
-  case DAM_MAGICAL:
+    case DAM_MAGICAL:
         if (monster_flags(m, MF_RES_MAGIC))
         {
             dam->amount /= monster_level(m);
@@ -1623,7 +1622,11 @@ monster *monster_damage_take(monster *m, damage *dam)
     case DAM_FIRE:
         if (monster_flags(m, MF_RES_FIRE))
         {
-            dam->amount /= 2;
+            /*
+             * The monster's fire resistance reduces the damage taken
+             * by 5% per monster level
+             */
+            dam->amount -= (guint)(((float)dam->amount / 100) * (monster_level(m) * 5));
             if (monster_in_sight(m))
             {
                 log_add_entry(nlarn->log, "The %s %sresists the flames.",

commit dff398e47feec6aac88480e6a1c54250625dccb7

Fire resistance: prevent uint wrap for high-level monsters
Issue introduced in 9801467c.

src/monsters.c
@@ -1626,7 +1626,9 @@ monster *monster_damage_take(monster *m, damage *dam)
              * The monster's fire resistance reduces the damage taken
              * by 5% per monster level
              */
-            dam->amount -= (guint)(((float)dam->amount / 100) * (monster_level(m) * 5));
+            dam->amount -= (guint)(((float)dam->amount / 100) *
+                 /* prevent uint wrap around for monsters with level > 20 */
+                 (min(monster_level(m), 20) * 5));
             if (monster_in_sight(m))
             {
                 log_add_entry(nlarn->log, "The %s %sresists the flames.",