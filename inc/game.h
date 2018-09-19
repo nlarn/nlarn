nlarn/nlarn

commit cfbed1336a2296f020b1f1d842a33daf2233509c

Prevent increasing max. HP with healing spell

src/player.c
@@ -2396,9 +2396,12 @@ effect *player_effect_add(player *p, effect *e)
                     guint amount = (p->hp_max * e->amount) / 100;
                     player_hp_gain(p, amount);
             }
-            else
+            else if (e->item)
             {
-                    /* Player's hp is at the max; increase max hp. */
+                    /*
+                     * Player's hp is at the max; increase max hp.
+                     * (only when drinking a potion)
+                     */
                     guint amount = e->amount;
 
                     effect_destroy(e);
@@ -2417,9 +2420,12 @@ effect *player_effect_add(player *p, effect *e)
                     guint amount = (p->mp_max * e->amount) / 100;
                     player_mp_gain(p, amount);
             }
-            else
+            else if (e->item)
             {
-                    /* Player's mp is at the max; increase max mp. */
+                    /*
+                     * Player's mp is at the max; increase max mp.
+                     * (only when drinking a potion)
+                     */
                     guint amount = e->amount;
 
                     effect_destroy(e);

src/potions.c
@@ -319,6 +319,7 @@ static int potion_with_effect(struct player *p, item *potion)
     if (potion_effect(potion) > ET_NONE)
     {
         effect *eff = effect_new(potion_effect(potion));
+        eff->item = potion;
 
         /* silly potion of giant strength */
         if (potion->id == PO_GIANT_STR)

commit fbb40b59cd9e77144d56bbba9b45686b1704d60f

Fix "Cure Blindness" spell
Bug introduced in cfbed133.

Closes #280

inc/game.h
@@ -29,7 +29,7 @@
 #define TIMELIMIT 30000 /* maximum number of moves before the game is called */
 
 /* internal counter for save file compatibility */
-#define SAVEFILE_VERSION    23
+#define SAVEFILE_VERSION    24
 
 /* the world as we know it */
 typedef struct game

src/player.c
@@ -2546,6 +2546,16 @@ effect *player_effect_add(player *p, effect *e)
                 log_add_entry(nlarn->log, "%s", effect_get_msg_start(e));
             else if (effect_get_amount(e) < 0 && effect_get_msg_stop(e))
                 log_add_entry(nlarn->log, "%s", effect_get_msg_stop(e));
+
+            /* If the effect was caused by a potion, delete the link
+             * between item and effect now, otherwise the effects caused
+             * by potions are not returned whenever player_effect_get()
+             * is used. (e.g. the spell "cure blindness" cannot cure
+             * blindness) anymore. */
+            if (e->item && ((item *)game_item_get(nlarn, e->item))->type == IT_POTION)
+            {
+                e->item = NULL;
+            }
         }
 
         if (str_orig != player_get_str(p))

src/potions.c
@@ -320,7 +320,8 @@ static int potion_with_effect(struct player *p, item *potion)
     if (potion_effect(potion) > ET_NONE)
     {
         effect *eff = effect_new(potion_effect(potion));
-        eff->item = potion;
+        /* allow referencing the effect back to the potion */
+        eff->item = potion->oid;
 
         /* silly potion of giant strength */
         if (potion->id == PO_GIANT_STR)