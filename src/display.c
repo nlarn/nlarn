609c5fa53271f8c85a432fc9591e0e686120baba
Joachim de Groot <jdegroot@web.de>
2018-12-17 21:39:34 +0100

    Fix numpad movement with PDCurses and num lock on

--- src/display.c
+++ src/display.c
@@ -138,6 +138,7 @@ void display_init()
             VERSION_MINOR, VERSION_PATCH, GITREV);
 
     PDC_set_title(window_title);
+    PDC_save_key_modifiers(FALSE);
     g_free(window_title);
 #endif
 
