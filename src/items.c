nlarn/nlarn

commit 8a82f0fbe0ca77a9ac02eb4b0ee9f205076978a9

Shatter potions when exposed to fire

src/items.c
@@ -1283,7 +1283,8 @@ static int material_affected(item_material_t mat, item_erosion_type iet)
     switch (iet)
     {
     case IET_BURN:
-        return (mat <= IM_BONE);
+        /* potions will shatter when exposed to fire */
+        return (mat <= IM_BONE || mat == IM_GLASS);
     case IET_CORRODE:
         return (mat == IM_IRON);
     case IET_RUST:
@@ -1335,10 +1336,22 @@ item *item_erode(inventory **inv, item *it, item_erosion_type iet, gboolean visi
     switch (iet)
     {
     case IET_BURN:
-        it->burnt++;
-        erosion_desc = "burn";
+        if (it->type == IT_POTION)
+        {
+            erosion_desc = "shatter";
+            destroyed = TRUE;
+        }
+        else if (item_material(it) == IM_GLASS)
+        {
+            /* other things made of glass are not affected */
+        }
+        else
+        {
+            it->burnt++;
+            erosion_desc = "burn";
 
-        if (it->burnt == 3) destroyed = TRUE;
+            if (it->burnt == 3) destroyed = TRUE;
+        }
         break;
 
     case IET_CORRODE:

commit cd8a856db98704b2eb6b1985645e78d9c34428cf

Fix breaking potions upon creation
Introduced with 8a82f0fb.

src/items.c
@@ -373,8 +373,9 @@ item *item_new_finetouch(item *it)
         }
     }
 
-    /* maybe the item is corroded */
-    if (item_is_corrodible(it->type) && chance(25))
+    /* maybe the item is corroded
+       Don't erode potions, as they break when exposed to fire */
+    if (it->type != IT_POTION && item_is_corrodible(it->type) && chance(25))
     {
         it = item_erode(NULL, it, rand_1n(IET_MAX), FALSE);
     }