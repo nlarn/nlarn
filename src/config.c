a7481b5d7b809b4e9ebbe1a83c5fc0c3e5fa102f
Joachim de Groot <jdegroot@web.de>
2025-03-12 21:13:02 +0100

    Remove command line option to set font size
    This is supposed to be an option that is configured from inside the
    game.

--- src/config.c
+++ src/config.c
@@ -92,9 +92,6 @@ void parse_commandline(int argc, char *argv[], struct game_config *config)
         { "auto-pickup", 'a', 0, G_OPTION_ARG_STRING, &config->auto_pickup,  "Item types to pick up automatically, e.g. '$*+'", NULL },
         { "no-autosave", 'N', 0, G_OPTION_ARG_NONE,   &config->no_autosave,  "Disable autosave", NULL },
         { "wizard",      'w', 0, G_OPTION_ARG_NONE,   &config->wizard,       "Enable wizard mode", NULL },
-#ifdef SDLPDCURSES
-        { "font-size",   'S', 0, G_OPTION_ARG_INT,    &config->font_size,    "Set font size", NULL },
-#endif
         { "userdir",     'D', 0, G_OPTION_ARG_FILENAME, &config->userdir,    "Alternate directory for config file and saved games", NULL },
         { "highscores",  'h', 0, G_OPTION_ARG_NONE,   &config->show_scores,  "Show highscores and exit", NULL },
         { "version",     'v', 0, G_OPTION_ARG_NONE,   &config->show_version, "Show version information and exit", NULL },
