cc3cbd704ede2e2d40f6e8595bd712e455e4bb23
Joachim de Groot <jdegroot@web.de>
2025-04-15 20:11:29 +0200

    Add missing assertion

--- inc/random.h
+++ inc/random.h
@@ -1,6 +1,6 @@
 /*
  * random.h
- * Copyright (C) 2009-2018 Joachim de Groot <jdegroot@web.de>
+ * Copyright (C) 2009-2025 Joachim de Groot <jdegroot@web.de>
  *
  * NLarn is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -36,7 +36,7 @@ guint32 rand_0n(guint32 n);
 /* returns a value x with m <= x < n. */
 static inline guint32 rand_m_n(guint32 m, guint32 n)
 {
-	g_assert(m < n);
+    g_assert(m < n);
     return rand_0n(n - m) + m;
 }
 
@@ -47,6 +47,7 @@ static inline guint32 rand_1n(guint32 n)
 
 static inline gboolean chance(guint32 percent)
 {
+    g_assert(percent < 101);
     return (percent >= rand_1n(101));
 }
 
